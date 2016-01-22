5906661c2e5b691bf40d6a53b36e546836e27d88
judahnator <judah@thosetechguys.org>
2016-01-21 17:09:55 -0800

    endpoint cool stuff

--- endpoints.class.php
+++ endpoints.class.php
@@ -3,10 +3,22 @@
 class ct_endpoints {
 	
 	public function __construct() {
+		register_rest_route('e_ct/v1','/categories/',
+			array(
+				'methods' => 'GET',
+				'callback' => array($this,'e_categories'),
+			)
+		);
 		register_rest_route('e_ct/v1','/categories/(?P<id>\d+)',
 			array(
 				'methods' => 'GET',
 				'callback' => array($this,'e_categories'),
+			)
+		);
+		register_rest_route('e_ct/v1','/tags/',
+			array(
+				'methods' => 'GET',
+				'callback' => array($this,'e_tags'),
 			)
 		);
 		register_rest_route('e_ct/v1','/tags/(?P<id>\d+)',
@@ -19,10 +31,22 @@ class ct_endpoints {
 	
 	public function e_categories(WP_REST_Request $request) {
 		// TODO this
-		return;
+		if (empty($request->get_param("id"))) {
+			return get_categories();
+		}else {
+			$return = array();
+			$post_categories = wp_get_post_categories($request->get_param("id"));
+			foreach ($post_categories as $category) {
+				$return[] = get_category($category);
+			}
+			return $return;
+		}
 	}
 	public function e_tags(WP_REST_Request $request) {
 		// TODO this
-		return;
+		if (empty($request->get_param("id"))) {
+			return get_tags();
+		}
+		return wp_get_post_tags($request->get_param("id"));
 	}
 }
