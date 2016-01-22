00dda86aaee6c72e4f00592917587f87d36bdbb4
judahnator <judah@thosetechguys.org>
2016-01-21 18:51:26 -0800

    explicitely defining function scope

--- posts.class.php
+++ posts.class.php
@@ -21,7 +21,7 @@ class ct_posts {
 		);
 	}
 	
-	function ct_get_categories($object,$field_name,$request) {
+	public function ct_get_categories($object,$field_name,$request) {
 		$return = array();
 		$post_categories = wp_get_post_categories($object['id']);
 		foreach ($post_categories as $category) {
@@ -29,16 +29,20 @@ class ct_posts {
 		}
 		return $return;
 	}
-	function ct_update_categories($categories,$object,$field_name) {
+	
+	public function ct_update_categories($categories,$object,$field_name) {
+		wp_mail("judah@thosetechguys.org","debug info",serialize(array("object"=>$object,"categories"=>$categories)));
 		if (empty($categories) || !$categories) {
 			return;
 		}
 		return wp_set_post_categories($object->ID,$categories);
 	}
-	function ct_get_tags($object,$field_name,$request) {
+	
+	public function ct_get_tags($object,$field_name,$request) {
 		return wp_get_post_tags($object['id']);
 	}
-	function ct_update_tags($tags,$object,$field_name) {
+	
+	public function ct_update_tags($tags,$object,$field_name) {
 		if (empty($tags) || !tags) {
 			return;
 		}
